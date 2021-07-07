c92408c6fcaebe92f462b0a73fd14155add14def
Ray Consulting <rayconsulting@shopeneur.com>
2021-07-06 23:04:41 -0400

    updated cron job reference

--- App_Data/jobs/triggered/cron/cron.php
+++ App_Data/jobs/triggered/cron/cron.php
@@ -27,7 +27,7 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-include "bootstrap.php";
+include "../../../../bootstrap.php";
 
 use Espo\Core\{
     Application,
